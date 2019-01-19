e02383dbeaa7cd7920fd80fa45d4b4c365a688b7
root <yaseenabbasrizvi@gmail.com>
2019-01-20 02:32:19 +0530

    added bootstrap and jobs are showing up in home

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import job
+
 # Create your views here.
+def home(request):
+    jobs = job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
